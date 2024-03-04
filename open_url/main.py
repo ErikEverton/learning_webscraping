16f52db4004f1b1682847a45ef2e1b9ec71335da
Erik <erik.everton444@gmail.com>
2024-03-04 08:16:12 -0300

    add some error handling

--- open_url/main.py
+++ open_url/main.py
@@ -1,6 +1,29 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 
-html = urlopen("https://www.google.com/")
-parsed_html = BeautifulSoup(html.read(), 'lxml')
-print(parsed_html.prettify())
+
+def get_link(parsed_html):
+    try:
+        title = parsed_html.body.a
+    except AttributeError as error:
+        return None
+    return title
+
+
+def main():
+    try:
+        html = urlopen("https://www.google.com/")
+    except HTTPError as error:
+        print(error)
+    except URLError as error:
+        print(error)
+    else:
+        parsed_html = BeautifulSoup(html.read(), 'lxml')
+        link = get_link(parsed_html=parsed_html)
+        if link:
+            print(link)
+        else:
+            print("Title could not be found")
+
+main()
